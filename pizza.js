524a4a9bbed1e1b68f189d81d8f18eac9dc79fe9
hulthe <joakim@hulthe.net>
2017-02-07 22:02:54 +0100

    Show the total number of orders

--- pizza.js
+++ pizza.js
@@ -56,6 +56,14 @@ if (Meteor.isClient) {
       });
       return _.sortBy(pizzas, function(pizza){ return -pizza.length; });
     },
+    items_total_length: function() {
+      var items = OrderItems.find({order: Session.get('order')}).fetch();
+      return items.length;
+    },
+    has_items: function() {
+      var items = OrderItems.find({order: Session.get('order')}).fetch();
+      return items.length > 0;
+    },
     error: function() {
       return Session.get('error');
     },
