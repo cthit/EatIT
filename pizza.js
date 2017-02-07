6675fdc95a371bd08d5fba54f32d081fb191f7d7
hulthe <joakim@hulthe.net>
2017-02-07 18:51:29 +0100

    Fix timer never displaying negative time on even minutes

--- pizza.js
+++ pizza.js
@@ -107,7 +107,7 @@ if (Meteor.isClient) {
   }
   function format_time(time) {
     var prefix = '';
-    if (time[1] < 0) {
+    if (time[0] < 0 || time[1] < 0) {
       prefix = '-';
     }
     time[0] = Math.abs(time[0]);
