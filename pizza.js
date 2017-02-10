153266915a9cc8ad13be0636b23520f050c4e50e
Feffe <c.fredrik.aberg@gmail.com>
2017-02-10 09:02:57 +0000

    Removed clipboard button, function now bound to link.

--- pizza.js
+++ pizza.js
@@ -94,26 +94,19 @@ if (Meteor.isClient) {
         OrderItems.remove({_id: id});
       }
     },
-    "click .copy-to-clipboard-button": function(e){
-
+    "click #share-link": function(e){
         // Create an auxiliary hidden input
         var aux = document.createElement("input");
-
         // Get the text from the element passed into the input
         aux.setAttribute("value", Session.get('url'));
-
         // Append the aux input to the body
         document.body.appendChild(aux);
-
         // Highlight the content
         aux.select();
-
         // Execute the copy command
         document.execCommand("copy");
-
         // Remove the input from the body
         document.body.removeChild(aux);
-
       }
   });
   function time_left() {
